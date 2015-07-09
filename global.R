73b5639086ef0d9f658a6542eb576b887fc314fa
Conor Lawless <cnr.lwlss@gmail.com>
2015-07-09 11:06:05 +0100

    Rearranging files and adding raw data

--- global.R
+++
@@ -1,4 +0,0 @@
-fc=read.delim("FunctionalComplexesTrimmed.txt",sep="\t",header=TRUE,stringsAsFactors=FALSE)
-fc=rbind(c("None","None","None",""),fc)
-choiceList=as.list(1:length(fc$Notes))
-names(choiceList)=fc$GroupName
